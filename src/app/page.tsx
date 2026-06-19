b3e67c5c66517022c201224457b749fc63a99908
indalacolourmoon <mohan@colourmoon.com>
2026-06-19 12:44:05 +0530

    feat: initialize homepage with site metadata and section components including icon asset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-'use client';
-
+import { Metadata } from 'next';
 import Hero from '@/components/HeroSection';
 import AboutPage from '@/app/about/about';
 import ProjectsPage from '@/app/projects/projects';
 import ContactPage from '@/app/contact/contact';
+
+export const metadata: Metadata = {
+  title: 'Mohan Kumar Indala | Full Stack Web Developer Portfolio',
+  description: 'Explore the portfolio of Mohan Kumar Indala, a passionate Full Stack Web Developer skilled in React, Next.js, Express, and SQL, building dynamic and high-performance web applications.',
+};
 
 export default function HomePage() {
   return (
